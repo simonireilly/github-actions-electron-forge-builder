d64e029d81ba95aae78f03205980ffb3367ce009
simonireilly@gmail.com <simonireilly@gmail.com>
2019-08-30 08:12:29 +0100

    Remove unsupported formats

--- src/index.js
+++ src/index.js
@@ -20,7 +20,9 @@ const createWindow = () => {
   mainWindow.loadURL(`file://${__dirname}/index.html`);
 
   // Open the DevTools.
-  mainWindow.webContents.openDevTools();
+  if (process.env.ENV === "develop") {
+    mainWindow.webContents.openDevTools();
+  }
 
   // Emitted when the window is closed.
   mainWindow.on('closed', () => {
